222238d944702821ab205e8137a5786fbe7e6ee1
ashwinh <35991970+ashwinh@users.noreply.github.com>
2018-02-19 13:21:27 +0530

    Create a41.c

---
+++ a41.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n=4,i;
+	char a[20];
+	printf("enter the string",a);
+	scanf("%d",a);
+	for(i=1;i<=n;i++);
+	{
+		printf("%s",a);
+	}
+	getch();
+  }
