d0decf7e328cfa9f806c4fd8e6b69ae7967986db
ashwinh <35991970+ashwinh@users.noreply.github.com>
2018-02-19 13:34:54 +0530

    Create a43.c

---
+++ a43.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+ char a[10],b[10];
+ printf("\n Enter the strings");
+ scanf("%s",a);
+ scanf("%s",b);
+ printf("\n the concatenated string is:%s%s",a,b);
+ getch();
+ }
