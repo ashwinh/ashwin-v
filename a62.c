0c96841bfcba39ee4bce8be6d62219b12e79532e
ashwinh <35991970+ashwinh@users.noreply.github.com>
2018-02-26 13:55:19 +0530

    Create a62.c

---
+++ a62.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if(n%10==0|n%10==1)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+getch();
+}
