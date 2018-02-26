f1f5a1da9527e79ad11624629bb70efa1a366828
ashwinh <35991970+ashwinh@users.noreply.github.com>
2018-02-26 13:29:14 +0530

    Create a57.c

---
+++ a57.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,t;
+a=10;
+b=20;
+{
+printf("elements before swapping");
+scanf("%d %d",&a,&b);
+t=a;
+a=b;
+b=t;
+printf("elements after swapping:%d %d",a,b);
+}
+getch();
+}
