9f916ca2929de7dc8203f39dc9c76693138017bc
ashwinh <35991970+ashwinh@users.noreply.github.com>
2018-02-26 13:51:07 +0530

    Create a61.c

---
+++ a61.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char r[20];
+int j,i;
+printf("enter the string");
+scanf("%s",&r[i]);
+printf("enter the number");
+scanf("%d",&j);
+for(i=0;i<j;i++)
+{
+printf("%c",r[i]);
+}
+getch();
+}
