e86fbd052f309b0396262167e50a2ee3d8c9c9a8
ashwinh <35991970+ashwinh@users.noreply.github.com>
2018-02-26 13:38:30 +0530

    Create a59.c

---
+++ a59.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a[50]={8,90,34,56,23,13,78,4,1,70};
+int max,i;
+max=a[0];
+for(i=1;i<=10;i++);
+{
+if(a[i]>max0
+{
+max=a[i];
+}
+printf("the maximum number is%d",max);
+}
+getch();
+}
