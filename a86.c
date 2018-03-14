de47945f991ab8712b2243bb1919d8049f658ac2
ashwinh <35991970+ashwinh@users.noreply.github.com>
2018-03-14 16:12:06 +0530

    Create a86.c

---
+++ a86.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<string.h>
+#inclyde<conio.h>
+int main()
+{
+char ch[30];
+int l,i,j,count=0;
+printf("Enter the string");
+scanf("%s",ch);
+l=strlen(ch);
+for(i=0;i<=l-1;i++)
+{
+for(j=i+1;j<=l;j++)
+ {
+ if(ch[i]==ch[j])
+ {
+count=1;
+}
+}
+}
+if(count==1)
+{
+printf("NO");
+}
+else
+{
+printf("YES");
+}
+getch();
+}
