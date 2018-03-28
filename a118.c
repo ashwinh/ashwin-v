c68c922526c71074eb7b78a702bc9307698c6dcc
ashwinh <35991970+ashwinh@users.noreply.github.com>
2018-03-28 15:44:33 +0530

    Create a118.c

---
+++ a118.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+n=int(input())
+if n<0:
+	print("positive numbers only")
+elif n==0:
+	print("factorial of zero is 1")
+else:
+	fact=1
+	for i in range(1,n+1):
+		fact = fact*i
+	print(fact);
+  return 0;
+  }
